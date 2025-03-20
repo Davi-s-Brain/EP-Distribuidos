bde54c68e0edd0fd20184b876ababdb47e68db87
Davi Batista <davidedeus28@hotmail.com>
2025-03-19 22:33:21 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,22 @@
+import inquirer
+from inquirer.themes import BlueComposure
+
+
+def main():
+    print("Olha o trabalho de Distribuídos ai")
+    escolhas = [inquirer.List("escolha", message="Escolha um comando", choices=[
+      "[1] Listar peers",
+      "[2] Obter peers",
+      "[3] Listar arquivos locais",
+      "[4] Buscar arquivos",
+      "[5] Exibir estatisticas",
+      "[6] Alterar tamanho de chunk",
+      "[9] Sair"
+    ])]
+    acao_escolhida = inquirer.prompt(escolhas, theme=BlueComposure())
+
+    if acao_escolhida["escolha"] == "[9] Sair":
+        exit(0)
+
+if __name__ == "__main__":
+    main()
