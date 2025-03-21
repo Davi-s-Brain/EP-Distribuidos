69aeeb23f6445051bbdd76a3d7d6a7d4caa4dda8
Davi Batista <davidedeus28@hotmail.com>
2025-03-21 17:32:15 -0300

    Criando um socket para cada peer

--- main.py
+++ main.py
@@ -5,6 +5,44 @@ from inquirer.themes import BlueComposure
 
 MAX_CONNECTIONS = 10
 
+
+class Peer:
+    def __init__(self, ip, port, shared_directory, status):
+        self.ip = ip
+        self.port = port
+        self.shared_directory = shared_directory
+        self.status = status
+
+    def __str__(self):
+        return f"Peer {self.ip}:{self.port} - {self.shared_directory}"
+
+    def __repr__(self):
+        return f"Peer {self.ip}:{self.port} - {self.shared_directory}"
+
+    @classmethod
+    def create_peer(cls, ip, port, shared_directory, status):
+        address = (ip, int(port))
+        peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        peer_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        peer_socket.bind(address)
+        peer_socket.listen(MAX_CONNECTIONS)
+
+        return cls(ip, port, shared_directory, status)
+
+
+def handle_file(path: str) -> list[str]:
+    neighbor = []
+    try:
+        with open(path, 'r') as file:
+            for file_neighbor in file.readlines():
+                formated_neighbor = file_neighbor.strip()
+                neighbor.append(formated_neighbor)
+    except IOError as error:
+        print(f"Error reading file {path}: {error}")
+        return []
+    return neighbor
+
+
 def main(args: list):
     params = args
     peer_ip_and_port = params[0]
@@ -13,26 +51,38 @@ def main(args: list):
 
     PEER_IP = socket.gethostbyname(peer_ip_and_port.split(":")[0])
     PEER_PORT = int(peer_ip_and_port.split(":")[1])
-    ADDR = (PEER_IP, PEER_PORT)
 
-    peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    peer_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    peer_socket.bind(ADDR)
-    peer_socket.listen(MAX_CONNECTIONS)
+    initial_peer = Peer.create_peer(
+        ip=PEER_IP, port=PEER_PORT, shared_directory=shared_directory, status="ONLINE")
+
+    for neighbor in handle_file(neighbor_list):
+        split_neighbor = neighbor.split(":")
+        neighbor_ip = split_neighbor[0]
+        neighbor_port = split_neighbor[1]
+
+        neighbor_peer = Peer.create_peer(
+            ip=neighbor_ip, port=neighbor_port, shared_directory=shared_directory, status="OFFLINE")
+
+        if neighbor_peer:
+            print(
+                f"Adicionando novo peer {neighbor_ip}:{neighbor_port} status OFFLINE")
+        else:
+            print(f"Erro na criação do peer")
 
     choices = [inquirer.List("choice", message="Escolha um comando", choices=[
-      "[1] Listar peers",
-      "[2] Obter peers",
-      "[3] Listar arquivos locais",
-      "[4] Buscar arquivos",
-      "[5] Exibir estatisticas",
-      "[6] Alterar tamanho de chunk",
-      "[7] Sair"
+        "[1] Listar peers",
+        "[2] Obter peers",
+        "[3] Listar arquivos locais",
+        "[4] Buscar arquivos",
+        "[5] Exibir estatisticas",
+        "[6] Alterar tamanho de chunk",
+        "[7] Sair"
     ])]
     selected_action = inquirer.prompt(choices, theme=BlueComposure())
 
     if selected_action["choice"] == "[9] Sair":
         exit(0)
+
 
 if __name__ == "__main__":
     args = sys.argv[1:]
