8a1c20e856a7906632251322c1bd89588aee3402
Davi Batista <davidedeus28@hotmail.com>
2025-03-22 13:31:34 -0300

    Criando os peers offline com a entrada do começo

--- main.py
+++ main.py
@@ -9,30 +9,42 @@ MAX_CONNECTIONS = 10
 
 
 class Peer:
-    def __init__(self, ip, port, shared_directory, status):
+    def __init__(self, ip, port, shared_directory, status, neighbors):
         self.ip = ip
         self.port = port
         self.shared_directory = shared_directory
         self.status = status
         self.clock = 0
-        self.start_server()
-
-    def __str__(self):
-        return f"Peer {self.ip}:{self.port} - {self.shared_directory}"
-
-    def __repr__(self):
-        return f"Peer {self.ip}:{self.port} - {self.shared_directory}"
+        self.neighbors = neighbors
 
     @classmethod
     def create_peer(cls, ip, port, shared_directory, status):
+        neighbors = []
+        last_port_digit = port[-1]
         address = (ip, int(port))
         peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         peer_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         peer_socket.bind(address)
         peer_socket.listen(MAX_CONNECTIONS)
 
-        return cls(ip, port, shared_directory, status)
-    
+        with open(f"./vizinhos/v{last_port_digit}_vizinhos.txt") as file_vizinhos:
+            neighbors_file = file_vizinhos.readlines()
+
+        for neighbor in neighbors_file:
+            formated_neighbor = neighbor.replace("\n", "")
+            formated_ip, formated_port = formated_neighbor.split(":")
+
+            new_neighbor = {"ip": formated_ip,
+                            "port": formated_port, "status": "OFFLINE"}
+
+            neighbors.append(new_neighbor)
+
+        for neighbor in neighbors:
+            print(
+                f"Adicionando novo peer {neighbor['ip']}:{neighbor['port']} status OFFLINE")
+
+        return cls(ip, port, shared_directory, status, neighbors)
+
     def start_server(self):
         def server_thread():
             server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -49,6 +61,7 @@ class Peer:
                 conn.close()
 
         threading.Thread(target=server_thread, daemon=True).start()
+
 
     def handle_command(self, command, conn):
         if command == "TIME":
@@ -88,7 +101,7 @@ def list_local_files(directory: str) -> None:
         files = os.listdir(directory)
     except Exception as error:
         print(f"Erro ao ler o diretório {directory}: {error}")
- 
+
     for file in files:
         print(f"{file}")
 
@@ -107,61 +120,41 @@ def verify_files_path(directory: str) -> bool:
     return True
 
 
-
-
 def main(args: list):
     params = args
     peer_ip_and_port = params[0]
-    neighbor_list = params[1]
     shared_directory = params[2]
+    selected_action = {"choice": ""}
 
     isPathValid = verify_files_path(shared_directory)
 
     if not isPathValid:
         exit(0)
 
-    ##PEER_IP = socket.gethostbyname(peer_ip_and_port.split(":")[0])
-    ##PEER_PORT = int(peer_ip_and_port.split(":")[1])
+    PEER_IP = socket.gethostbyname(peer_ip_and_port.split(":")[0])
+    PEER_PORT = peer_ip_and_port.split(":")[1]
 
-    initial_peer = Peer.create_peer(
+    Peer.create_peer(
         ip=PEER_IP, port=PEER_PORT, shared_directory=shared_directory, status="ONLINE")
 
-    for neighbor in handle_file(neighbor_list):
-        split_neighbor = neighbor.split(":")
-        neighbor_ip = split_neighbor[0]
-        neighbor_port = split_neighbor[1]
+    while selected_action["choice"] != "[7] Sair":
+        choices = [inquirer.List("choice", message="Escolha um comando", choices=[
+            "[1] Listar peers",
+            "[2] Obter peers",
+            "[3] Listar arquivos locais",
+            "[4] Buscar arquivos",
+            "[5] Exibir estatisticas",
+            "[6] Alterar tamanho de chunk",
+            "[7] Sair"
+        ])]
+        selected_action = inquirer.prompt(choices, theme=BlueComposure())
 
-        ##perguntar pro davi
-        neighbor_peer = Peer.create_peer(
-            ip=neighbor_ip, port=neighbor_port, shared_directory=shared_directory, status="OFFLINE")
-        ##aqui acho que só precisa adicionar na lista de vizinhos mesmo
-        if neighbor_peer:
-            print(
-                f"Adicionando novo peer {neighbor_ip}:{neighbor_port} status OFFLINE")
-        else:
-            print(f"Erro na criação do peer")
-    
+        if selected_action["choice"] == "[3] Listar arquivos locais":
+            list_local_files(shared_directory)
 
-    choices = [inquirer.List("choice", message="Escolha um comando", choices=[
-        "[1] Listar peers",
-        "[2] Obter peers",
-        "[3] Listar arquivos locais",
-        "[4] Buscar arquivos",
-        "[5] Exibir estatisticas",
-        "[6] Alterar tamanho de chunk",
-        "[7] Sair"
-    ])]
-    selected_action = inquirer.prompt(choices, theme=BlueComposure())
-  
-    if selected_action["choice"] == "[3] Listar arquivos locais":
-        list_local_files(shared_directory)
-
-    elif selected_action["choice"] == "[9] Sair":
+        elif selected_action["choice"] == "[9] Sair":
             exit(0)
-    elif selected_action["choice"] == "[1] Listar peers": ##TODO: to só testando!!!!!!!!!!!!!
-        command = "TIME"
-        initial_peer.send_command(command,PEER_IP,9002)
-    
+
 
 if __name__ == "__main__":
     args = sys.argv[1:]
