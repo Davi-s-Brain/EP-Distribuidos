7e21a7e63970533f820e4cc003081999d655cbac
Filipe Valeriano <valeriano333@outlook.com>
2025-03-22 13:16:12 -0300

    conversação entre peers e mudanças na classe

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import socket
 import inquirer
 from inquirer.themes import BlueComposure
+import threading
 
 MAX_CONNECTIONS = 10
 
@@ -14,6 +15,7 @@ class Peer:
         self.shared_directory = shared_directory
         self.status = status
         self.clock = 0
+        self.start_server()
 
     def __str__(self):
         return f"Peer {self.ip}:{self.port} - {self.shared_directory}"
@@ -30,7 +32,43 @@ class Peer:
         peer_socket.listen(MAX_CONNECTIONS)
 
         return cls(ip, port, shared_directory, status)
+    
+    def start_server(self):
+        def server_thread():
+            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server.bind(('', self.port))
+            server.listen()
 
+            ip = socket.gethostbyname(socket.gethostname())
+            print(f"[Servidor] Escutando em {ip}:{self.port}")
+
+            while True:
+                conn, addr = server.accept()
+                data = conn.recv(1024).decode()
+                self.handle_command(data, conn)
+                conn.close()
+
+        threading.Thread(target=server_thread, daemon=True).start()
+
+    def handle_command(self, command, conn):
+        if command == "TIME":
+            print("Recebi um Time")
+        elif command.startswith("ECHO "):
+            print("Recebi um echo")
+        else:
+            response = "Comando desconhecido"
+        ##conn.sendall(response.encode())
+
+    def send_command(self, command, ip, port):
+        #ip,port = command.split(":", 1)
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((ip, port))
+                s.sendall(command.encode())
+                #response = s.recv(1024).decode()
+                print(f"Encaminhando mensagem {command} para {ip}{port}")
+        except Exception as e:
+            print(f"[Erro] Não foi possível conectar com {ip}:{port} - {e}")
 
 def handle_file(path: str) -> list[str]:
     neighbor = []
@@ -69,6 +107,8 @@ def verify_files_path(directory: str) -> bool:
     return True
 
 
+
+
 def main(args: list):
     params = args
     peer_ip_and_port = params[0]
@@ -86,21 +126,23 @@ def main(args: list):
     initial_peer = Peer.create_peer(
         ip=PEER_IP, port=PEER_PORT, shared_directory=shared_directory, status="ONLINE")
 
-    for neighbor in handle_file(neighbor_list):
+    '''for neighbor in handle_file(neighbor_list):
         split_neighbor = neighbor.split(":")
         neighbor_ip = split_neighbor[0]
         neighbor_port = split_neighbor[1]
 
+        ##perguntar pro davi
         neighbor_peer = Peer.create_peer(
             ip=neighbor_ip, port=neighbor_port, shared_directory=shared_directory, status="OFFLINE")
-
+        ##aqui acho que só precisa adicionar na lista de vizinhos mesmo
         if neighbor_peer:
             print(
                 f"Adicionando novo peer {neighbor_ip}:{neighbor_port} status OFFLINE")
         else:
             print(f"Erro na criação do peer")
+    '''
 
-    choices = [inquirer.List("choice", message="Escolha um comando", choices=[
+    '''choices = [inquirer.List("choice", message="Escolha um comando", choices=[
         "[1] Listar peers",
         "[2] Obter peers",
         "[3] Listar arquivos locais",
@@ -110,13 +152,18 @@ def main(args: list):
         "[7] Sair"
     ])]
     selected_action = inquirer.prompt(choices, theme=BlueComposure())
+    ''
+    '''
+    '''
+        if selected_action["choice"] == "[3] Listar arquivos locais":
+            list_local_files(shared_directory)
 
-    if selected_action["choice"] == "[3] Listar arquivos locais":
-        list_local_files(shared_directory)
-
-    elif selected_action["choice"] == "[9] Sair":
-        exit(0)
-
+        elif selected_action["choice"] == "[9] Sair":
+            exit(0)
+        elif selected_action["choice"] == "[1] Listar peers": ##TODO: to só testando!!!!!!!!!!!!!
+            command = "TIME"
+            initial_peer.send_command(command,PEER_IP,9002)
+    '''
 
 if __name__ == "__main__":
     args = sys.argv[1:]
